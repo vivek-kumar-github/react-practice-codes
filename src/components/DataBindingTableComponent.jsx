2b094d3b1b0e81b12b2cd24ad885bb039de2a030
Vivek Kumar <viveksingh599999@gmail.com>
2025-12-30 11:19:45 +0530

    Added DataBindingTableComponent

---
+++ src/components/DataBindingTableComponent.jsx
@@ -0,0 +1,33 @@
+function DataBindingTableComponent() {
+    const products = [
+        {Name : "Samsung TV", Price : 56000.44},
+        {Name : "Nike Casuals", Price : 4200.44}
+    ]
+    return (
+        <>
+        <div className="container">
+            <h2>Product Table</h2>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product =>
+                            <tr>
+                                <td>{product.Name}</td>
+                                <td>{product.Price}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+        </>
+    )
+}
+
+export default DataBindingTableComponent
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import DataBindingComponent2 from './components/DataBindingComponent2.jsx'
 import DataBindingComponent1 from './components/DataBindingComponent1.jsx'
+import DataBindingTableComponent from './components/DataBindingTableComponent.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     <DataBindingComponent1 />
     <DataBindingComponent2 />
+    <DataBindingTableComponent />
   </StrictMode>,
 )
